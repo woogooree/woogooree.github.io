b1d3bd296be260c0ffe7afe7d0c9bf6e286c88c0
woogooree <woogooree@gmail.com>
2024-05-21 18:54:01 +0900

    gatsby-meta-config.js 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `woogooree`,
+  description: `우구리 블로그`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://woogooree.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `심우진`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['기록을 좋아하는', '적극적으로 일하는', '성장하고 있는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
+      github: `https://github.com/woogooree`, // `https://github.com/zoomKoding`,
       linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      email: `woogooree@gmail.com`, // `zoomkoding@gmail.com`,
     },
   },
 
@@ -43,13 +43,24 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
+        date: '2017.03 ~ 2023.08',
+        activity: '경상대학교 산림환경자원학과 (졸업)',
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2024.03 ~ 2023.12',
+        activity: '한국폴리텍대학 진주캠퍼스 AI소프트웨어과',
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2024.05',
+        activity: '개인 블로그 운영 시작',
+      },
+      {
+        date: '2024.06',
+        activity: '정보처리기사 취득',
       },
     ],
 
@@ -72,15 +83,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '2024 ICT멘토링 한이음공모전',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '2024.03 ~ 2024.11 전력관리 플랫폼 KEPSI 프로젝트',
+        techStack: ['flutter', 'Firebase', 'Embedded'],
+        thumbnailUrl: '',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: '',
+          github: '',
+          demo: '',
         },
       },
     ],
