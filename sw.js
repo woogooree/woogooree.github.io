0de5293b4fc5a831d9778cea8b776af085cd9505
woogooree <woogooree@gmail.com>
2024-06-17 10:54:13 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9d338aeed658d3f0a211.js"
+    "url": "webpack-runtime-3a67f08a01c2896a8e96.js"
   },
   {
     "url": "styles.403d7a8a431a9dbd54c6.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "96a2723dd1c99707a841866f7de9e275"
+    "revision": "5438c703f21c04c319f7251ec162113a"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
